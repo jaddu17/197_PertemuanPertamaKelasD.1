74668c6f822927420a8e7536ce214546e7609791
shafa <shafa@DESKTOP-KMRCV2Q>
2024-02-27 11:16:31 +0700

    Add project files.

---
+++ PertemuanPertamaKelasD.1/PertemuanPertamaKelasD.1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{//begin
+    
+    //numeric nAlas, nTinggi, nLuas
+    //display 'Masukkan Alasnya='
+    //accept nAlas
+    //display 'Masukkan Tingginya='
+    //accept nTinggi
+    //compute nLuas = 1/2 * nAlas * nTinggi
+    //display 'Luasnya adalah = ' + nLuas
+    
+    int nAlas, nTinggi;
+    double nLuas;
+        cout << "Masukkan Alasnya= ";
+        cin >> nAlas;
+        cout << "Masukkan Tingginya= ";
+        cin >> nTinggi;
+        nLuas = 0, 5 * (nAlas * nTinggi);
+        cout << "Luasnya adalah= " << nLuas << endl;
+
+
+        system("pause");
+
+}//end 
+
+
